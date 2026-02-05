b462c6950e090615d67cd3353854fc813ca2d566
remadex <s.cardon@outlook.com>
2026-02-05 16:17:47 +0100

    fix: not right key of localStorage

--- @apps/front/app/utils/set-theme.ts
+++ @apps/front/app/utils/set-theme.ts
@@ -1,7 +1,7 @@
 export default function setTheme() {
   let theme = 'nord';
   if (typeof localStorage !== 'undefined') {
-    theme = localStorage.getItem('doc-app-theme') ?? 'nord';
+    theme = localStorage.getItem('tpk-theme') ?? 'nord';
   }
   if (typeof document !== 'undefined') {
     document.documentElement.setAttribute('data-theme', theme);
